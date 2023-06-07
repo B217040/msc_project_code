2523bdc1d5a50f1b850a9492aa780558eac82746
Paige Champken <ken@Paiges-MacBook-Air.local>
2023-06-07 21:38:22 +0100

    test for interactive node

--- test_interactive.py
+++ test_interactive.py
@@ -1,2 +1,2 @@
-with open('~/work/tc046/tc046/pchamp/results/test_textfile.txt', 'a') as f:
+with open('./work/tc046/tc046/pchamp/results/test_textfile.txt', 'a') as f:
     f.write('testing script on interactive node')
