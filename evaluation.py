266f17d5810cedb08ef367e42194c577ce0bbc33
B217040 <s2435904@ed.ac.uk>
2023-06-09 15:31:08 +0100

    'evaluation script updates'

--- evaluation.py
+++ evaluation.py
@@ -1,25 +1,38 @@
+#! /usr/bin/python
+
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from datasets import load_dataset
+from dataset_query import load_data
+import torch
+from evaluate import load
 
-# load model and processor
-processor = WhisperProcessor.from_pretrained("openai/whisper-base")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base")
-model.config.forced_decoder_ids = None
+def load_model(model_size):
+    processor = WhisperProcessor.from_pretrained(f"openai/whisper-{model_size}") #load whisper processer
+    model = WhisperForConditionalGeneration.from_pretrained(f"openai/whisper-{model_size}").to('cuda')
+    model.config.forced_decoder_ids = None # pairs of integers which indicates a mapping from generation indices to token indices
+    return processor, model
 
-# load dummy dataset and read audio files
 
-ds = load_dataset("google/fleurs", "clean", split="validation")
+def make_prediction(sample):
+    audio = sample["audio"]
+    input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
+    # return_tensors from WhisperFeatureExtractor class - pt is PyTorch format
+    batch["reference"] = processor.tokenizer._normalize(batch['text'])
 
-sample = ds[0]["audio"]
 
-input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features
+    with torch.no_grad():
+        predicted_ids = model.generate(input_features.to("cuda"))[0]
 
-# generate token ids
+    transcription = processor.decode(predicted_ids)
+    batch["prediction"] = processor.tokenizer._normalize(transcription)
 
-predicted_ids = model.generate(input_features)
+    return batch
 
-# decode token ids to text
 
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
+result = librispeech_test_clean.map(make_predicton) #map applies function to all samples in dataset
+wer = load("wer")
+print(100 * wer.compute(references=result["reference"], predictions=result["prediction"]))
 
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+if __name__ == '__main__':
+    eval_data = load_dataset('cy_gb')['test']
+    processor, model = load_model('tiny')
